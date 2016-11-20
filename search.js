b9500542cc5766b93af3a9700fe2c7721ae9a410
KumanshF <kumansh.furia@gmail.com>
2016-11-20 17:19:35 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,23 @@
+
+
+function showLoading() {
+    // remove existing loaders
+    
+    $('.loading-container').remove();
+    $('<div id="orrsLoader" class="loading-container"><div><div class="mdl-spinner mdl-js-spinner is-active"></div></div></div>').appendTo("body");
+    
+    componentHandler.upgradeElements($('.mdl-spinner').get());
+    setTimeout(function () {
+        $('#orrsLoader').css({opacity: 1});
+    }, 1);
+}
+
+function hideLoading() {
+    $('#orrsLoader').css({opacity: 0});
+    setTimeout(function () {
+        $('#orrsLoader').remove();
+    }, 400);
+    
+}
+
+
